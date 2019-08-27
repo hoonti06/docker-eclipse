a312b20787141d92d05f22543a5caafe421dc6f4
hoon <hoonti06@gmail.com>
2019-08-27 19:33:30 +0900

    Modify remote-debug-tomcat.sh

--- remote-debug-tomcat.sh
+++ remote-debug-tomcat.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# param1($1) : project's absolute path
-# param2($2) : docker instance name
+# project's absolute path
+PROJECT_PATH=~/data-docker/docker-eclipse/eclipse-workspace/simple_jsp 
 
-# e.g. ./remote-debug-tomcat.sh $PWD/simple_jsp_proj remote-debug-instance
+# docker instance name
+INSTANCE_NAME=remote-debug-tomcat 
 
+PORT=8000
+TOMCAT_VERSION=8
 
-docker rm -f $2
+docker rm -f ${INSTANCE_NAME}
 
-docker run --name=$2 -d \
--v $1:/usr/local/tomcat/webapps/proj \
--e JPDA_ADDRESS=8000 \
+docker run --name=${INSTANCE_NAME} -d \
+-v ${PROJECT_PATH}:/usr/local/tomcat/webapps/proj \
+-e JPDA_ADDRESS=${PORT} \
 -e JPDA_TRANSPORT=dt_socket \
--e DEBUG_OPTS="-Xdebug -Xrunjdwp:transport:transport=dt_socket,adress=4000,server=y,suspend=n" \
+-e DEBUG_OPTS="-Xdebug -Xrunjdwp:transport:transport=dt_socket,address=${PORT},server=y,suspend=n" \
 --network host \
-tomcat:8 catalina.sh jpda run
+tomcat:${TOMCAT_VERSION} catalina.sh jpda run
